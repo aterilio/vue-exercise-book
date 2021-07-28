e5588286c7b8028b2c9c9e420c72199bbb43a42c
심정미 <lina@aterilio.kr>
2021-07-28 23:08:22 +0900

    feat: vue study until 03

---
+++ chap3/quiz3/js/app.js
@@ -0,0 +1,23 @@
+// todo #1
+// register 'sibling-component'
+// options : template, props
+
+Vue.component('sibling-component', {
+    props: ['propsSibling'],
+    template: '<p>{{ propsSibling }}</p>'
+});
+
+Vue.component('child-component', {
+    props: ['propsdata'],
+    template: '<p>{{ propsdata }}</p>'
+});
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue! passed from Parent Component',
+        // todo #2
+        // add data property & value -> props for sibling-component
+        anotherMessage: 'test message for sibling component'
+    }
+})
